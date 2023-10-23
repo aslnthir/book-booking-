3c5b23a1fa5b8138c7364861ed6cf265a116ddd7
aslnthir <aslnthir@gmail.com>
2023-10-24 01:40:43 +0200

    icerik

---
+++ book/book/Controllers/AktivitetsArkivController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace book.Controllers
+{
+    public class AktivitetsArkivController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+} 
---
+++ book/book/Models/AktivitetsArkiv.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace book.Models
+{
+	public class AktivitetsArkiv
+	{
+		public AktivitetsArkiv()
+		{
+		}
+	}
+}
+
